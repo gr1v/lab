3055340c19f44f70c4b6953fb903c2b68d1f9b0f
gr1v <vasyagrickov@gmail.com>
2025-10-16 12:55:11 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,10 @@
+#Создадим список на python
+
+ld = ['Pizza', 'Apple', 'Pineapple']
+#Создадим пустой список STACK
+
+stack = list()
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print(stack)
